1f12db90417d77e835fe6ea1753b1c9e952dc223
asungy <62207329+asungy@users.noreply.github.com>
2024-04-20 01:38:52 -0400

    First commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, ... }:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        avocado = (import inputs.nixpkgs {inherit system;}).vimUtils.buildVimPlugin {
+          name = "avocado";
+          src = ./.;
+        };
+
+        nvim-overlay = prev: final: {
+          dev-nvim = inputs.nixvim.legacyPackages.${system}.makeNixvim {
+            extraPlugins = [avocado];
+          };
+        };
+
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [nvim-overlay];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            dev-nvim
+          ];
+        };
+      }
+    );
+}
